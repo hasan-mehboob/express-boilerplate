b170babd3c197198c4c9e041b5761b2f244ed21a
muzammil-tariq <muzammil413tariq@gmail.com>
2022-04-21 09:17:50 +0000

    security-questions:logout

--- src/modules/users/actions/get/user.logout.action.js
+++ src/modules/users/actions/get/user.logout.action.js
@@ -1,5 +1,18 @@
 module.exports = async (req, res, next) => {
   try {
+    const { user } = req;
+    const agent = useragent.parse(req.headers["user-agent"]);
+    await models.UserDevices.destroy({
+      where: {
+        userId: user.id,
+        deviceType: agent.os.toString(),
+      },
+    });
+    res.json({
+      status: 200,
+      message: messages.success,
+      data: null,
+    });
   } catch (error) {
     next(error);
   }
