0ed8ef384cc3f57b46dc84d6aa687871c9b1b786
muzammil-tariq <muzammil413tariq@gmail.com>
2022-04-29 14:17:10 +0500

    auth-refresh-token:jwt update

--- src/middlewares/jwt.middleware.js
+++ src/middlewares/jwt.middleware.js
@@ -3,21 +3,12 @@ const roleModel = {
   admins: models.Admins,
 };
 router.use(function (req, res, next) {
-  let isChecked = 0;
+  let isModelExist = 0;
   for (let route in roleModel) {
-    if (req.path.search(`${route}/`) != -1 && !isChecked) {
+    if (req.path.search(`${route}/`) != -1 && !isModelExist) {
       req.roleModel = roleModel[route];
-      isChecked = 1;
+      isModelExist = 1;
     }
-  }
-  if (!req.roleModel) {
-    try {
-      const jwtPayload = utils.token.verifyToken({
-        token: req.cookies.accessToken,
-        secret: auth.accessToken.secret,
-      });
-      req.roleModel = roleModel[jwtPayload.model];
-    } catch (error) {}
   }
 
   passport.authenticate("jwt", { session: false }, function (err, user, info) {
