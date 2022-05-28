033c83a75573eb3ae91580d64177c6cf08edd75f
muzammil-tariq <muzammil413tariq@gmail.com>
2022-05-28 08:44:31 +0000

    feature/auth-refresh-token:update news letter

--- src/modules/newsLetterSubscribers/actions/post/newsLetterSubscriber.create.action.js
+++ src/modules/newsLetterSubscribers/actions/post/newsLetterSubscriber.create.action.js
@@ -1,20 +1,24 @@
 module.exports = async (req, res, next) => {
-  const { body: payload } = req;
+  const {
+    body: { email, firstName, lastName },
+  } = req;
   try {
     const prevNewsLetterSubscriber = await models.NewsLetterSubscribers.findOne(
       {
         where: {
-          email: payload.email,
+          email,
         },
       }
     );
     if (prevNewsLetterSubscriber)
       return next(createError(400, messages.emailExists));
     const newsLetter = await models.NewsLetterSubscribers.create({
-      email: payload.email,
+      email,
+      firstName,
+      lastName,
     });
     libs.email_service.commingSoonLeads({
-      email: payload.email,
+      email,
     });
     return res.json({
       status: 200,
